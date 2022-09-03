9c917661c7bbe053a7db23ba3a64990d10cb63cd
Danny Li <dli0906@gmail.com>
2022-09-03 18:11:08 -0400

    More options - can delete columns and rename

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState, useCallback} from 'react'
+import {useState, useCallback, useEffect} from 'react'
 import RadarChart from 'react-svg-radar-chart'
 import toImg from 'react-svg-to-image'
 
@@ -31,11 +31,17 @@ function App() {
   }
 
   const [dataState, setDataState] = useState(initData)
+  const [captionsState, setCaptions] = useState(captions)
 
-  const handleDataChange = useCallback(
+  const captionsArray = Object.values(captionsState)
+
+  useEffect(() => {
+    console.log(captionsState)
+  }, [captionsState])
+
+  const handleDataNumberChange = useCallback(
     (e) => {
       const {name, value} = e.target
-
       if (dataState && dataState[0]?.data) {
         const newDataState = dataState.map((object) => {
           const updatedObject = (object.data[name] = Number(value) / 5)
@@ -48,68 +54,96 @@ function App() {
     [dataState]
   )
 
-  const onClick = () => toImg('svg', 'transparent-radar-chart')
+  const handleDataCaptionChange = useCallback(
+    (e) =>
+      setTimeout(() => {
+        const {name, value} = e.target
+        let newDataState, newCaptionsState
+
+        if (dataState && dataState[0]?.data) {
+          newDataState = [...dataState]
+          newDataState[0].data[value.toLowerCase()] = 0
+          if (captionsState) {
+            newCaptionsState = {...captionsState}
+            newCaptionsState[value.toLowerCase()] =
+              value[0].toUpperCase() + value.slice(1)
+          }
+          delete newDataState[0].data[name]
+          delete newCaptionsState[name]
+
+          setCaptions(newCaptionsState)
+          setDataState(newDataState)
+        }
+      }, 3000),
+    [dataState, captionsState]
+  )
+
+  const handleDelete = useCallback(
+    (e) => {
+      const {name, value} = e.target
+      let newDataState = [...dataState]
+      let newCaptionsState = {...captionsState}
+
+      delete newDataState[0].data[value.toLowerCase()]
+      delete newCaptionsState[value.toLowerCase()]
+      console.log(newCaptionsState, newDataState)
+      setCaptions(newCaptionsState)
+      setDataState(newDataState)
+    },
+    [dataState, captionsState]
+  )
+  const onClick = () =>
+    toImg('svg', 'transparent-radar-chart', {scale: 3, quality: 1})
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>Radar Chart</h2>
-        <div style={{display: 'flex', flexDirection: 'column'}}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
           <RadarChart
             id={'transparent-radar-chart-id'}
-            captions={captions}
+            captions={captionsState}
             data={dataState}
             options={options}
             size={450}
           />
-          {dataState && (
-            <div className="caption-list">
-              <div className="caption-item">
-                <label className="caption-label">Food:</label>
-                <input
-                  type="number"
-                  onChange={handleDataChange}
-                  name="food"
-                  min="0"
-                  step="1"
-                  max="5"
-                />
-              </div>
-              <div className="caption-item">
-                <label className="caption-label">Price:</label>
-                <input
-                  type="number"
-                  onChange={handleDataChange}
-                  name="price"
-                  min="0"
-                  step="1"
-                  max="5"
-                />
-              </div>
-              <div className="caption-item">
-                <label className="caption-label">Service:</label>
-                <input
-                  type="number"
-                  onChange={handleDataChange}
-                  name="service"
-                  min="0"
-                  step="1"
-                  max="5"
-                />
-              </div>
-              <div className="caption-item">
-                <label className="caption-label">Environment:</label>
-                <input
-                  type="number"
-                  onChange={handleDataChange}
-                  name="environment"
-                  min="0"
-                  step="1"
-                  max="5"
-                />
-              </div>
-            </div>
-          )}
+          <div className="caption-list">
+            {dataState &&
+              captionsArray.length > 0 &&
+              captionsArray.map((caption, index) => (
+                <div className="caption-item" key={`${caption}-${index}`}>
+                  <input
+                    className="caption-label"
+                    defaultValue={caption}
+                    onChange={handleDataCaptionChange}
+                    name={caption.toLowerCase()}
+                  />
+                  <span>:</span>
+                  <input
+                    type="number"
+                    onChange={handleDataNumberChange}
+                    name={caption.toLowerCase()}
+                    min="0"
+                    step="1"
+                    max="5"
+                  />
+                  <button
+                    className="delete-button"
+                    onClick={handleDelete}
+                    value={caption}
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))}
+          </div>
+
           <div>
             <button className="btn striped-shadow dark" onClick={onClick}>
               <span>Save Image</span>
